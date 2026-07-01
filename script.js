45f58fd5aeb2a0d3d986709ed171ec56ac59991d
band1999abc <band1999.abc@gmail.com>
2026-07-02 02:41:22 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+const hour = new Date().getHours();
+
+const sub = document.querySelector(".sub");
+
+if (hour >= 6 && hour < 12) {
+    sub.textContent = "Good morning.";
+} else if (hour >= 12 && hour < 18) {
+    sub.textContent = "Welcome.";
+} else if (hour >= 18 && hour < 20) {
+    sub.textContent = "Take a break.";
+} else {
+    sub.textContent = "Good evening.";
+}
