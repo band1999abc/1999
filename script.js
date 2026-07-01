0ad90d341dea441eab1080a98bb802aa26748869
band1999abc <band1999.abc@gmail.com>
2026-07-02 02:46:08 +0900

    Update script.js

--- script.js
+++ script.js
@@ -2,12 +2,18 @@ const hour = new Date().getHours();
 
 const sub = document.querySelector(".sub");
 
-if (hour >= 6 && hour < 12) {
-    sub.textContent = "Good morning.";
-} else if (hour >= 12 && hour < 18) {
-    sub.textContent = "Welcome.";
-} else if (hour >= 18 && hour < 20) {
-    sub.textContent = "Take a break.";
-} else {
-    sub.textContent = "Good evening.";
+if (!sub) {
+    // .sub がなければ何もしない
+} else if (window.location.pathname.endsWith("index.html") || window.location.pathname.endsWith("/1999/")) {
+
+    if (hour >= 6 && hour < 12) {
+        sub.textContent = "Good morning.";
+    } else if (hour >= 12 && hour < 18) {
+        sub.textContent = "Welcome.";
+    } else if (hour >= 18 && hour < 20) {
+        sub.textContent = "Take a break.";
+    } else {
+        sub.textContent = "Good evening.";
+    }
+
 }
