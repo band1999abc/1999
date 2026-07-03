a3f11f53e79e1d8bc60d41ee93803abf9c2aec9e
band1999abc <band1999.abc@gmail.com>
2026-07-04 02:39:46 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,1 @@
-const hour = new Date().getHours();
-
-const sub = document.querySelector(".sub");
-
-if (!sub) {
-  return;
-}
-
-if (hour >= 6 && hour < 12) {
-  sub.textContent = "Good morning.";
-} else if (hour >= 12 && hour < 18) {
-  sub.textContent = "Welcome.";
-} else if (hour >= 18 && hour < 20) {
-  sub.textContent = "Take a break.";
-} else {
-  sub.textContent = "Good evening.";
-}
+document.querySelector(".sub").textContent = "Good evening.";
