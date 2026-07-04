617e2bf9dbd4823622d32dd5dd9efeb4739b38b2
band1999abc <band1999.abc@gmail.com>
2026-07-04 13:48:04 +0900

    Refactor time-based greeting conditions

--- script.js
+++ script.js
@@ -5,9 +5,9 @@ if (sub) {
 
   if (hour >= 6 && hour < 10) {
     sub.textContent = "Good morning.";
-  } else if (hour >= 10 && hour < 18) {
+  } else if (hour >= 10 && hour < 15) {
     sub.textContent = "Welcome.";
-  } else if (hour >= 18 && hour < 20) {
+  } else if (hour >= 15 && hour < 17) {
     sub.textContent = "Take a break.";
   } else {
     sub.textContent = "Good evening.";
