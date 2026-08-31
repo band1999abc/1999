f899698f8ccc417d8b2c9703b5e8a46734a2bf6f
band1999abc <band1999.abc@gmail.com>
2026-08-31 21:19:19 +0900

    Allow MP3 extension when MIME varies

--- music-upload-client.bundle.js
+++ music-upload-client.bundle.js
@@ -6518,7 +6518,7 @@ ${newlined}
   async function uploadMusicBlob(file, musicId, onProgress) {
     if (!(file instanceof File)) throw new Error("MP3\u30D5\u30A1\u30A4\u30EB\u3092\u9078\u629E\u3057\u3066\u304F\u3060\u3055\u3044");
     if (!musicId) throw new Error("Music ID \u304C\u3042\u308A\u307E\u305B\u3093");
-    if (file.type && file.type !== "audio/mpeg") {
+    if (file.type && file.type !== "audio/mpeg" && !/\.mp3$/i.test(file.name || "")) {
       throw new Error("MP3 \u30D5\u30A1\u30A4\u30EB\u306E\u307F\u30A2\u30C3\u30D7\u30ED\u30FC\u30C9\u3067\u304D\u307E\u3059");
     }
     const adminToken = sessionStorage.getItem("admin_token") || "";
